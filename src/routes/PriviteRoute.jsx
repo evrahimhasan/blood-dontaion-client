49eea859405659e1496f595abd8543ba2467ef97
evrahimhasan <evrahimhasanemran.com>
2025-12-18 19:51:56 +0600

    added loading spinner

--- src/routes/PriviteRoute.jsx
+++ src/routes/PriviteRoute.jsx
@@ -1,12 +1,13 @@
 import React, { use } from 'react';
 import { AuthContext } from '../provider/AuthProvider';
 import { Navigate } from 'react-router';
+import LoadingSpinner from '../components/loadingSpinner/LoadingSpinner';
 
 const PriviteRoute = ({ children }) => {
     const { user, loading, roleLoading, userStatus } = use(AuthContext)
 
     if (loading || roleLoading) {
-        return <p>Loading........</p>
+        return <LoadingSpinner></LoadingSpinner>
     }
     if (!user || userStatus !== 'active') {
         return <Navigate to={'/login'}></Navigate>
